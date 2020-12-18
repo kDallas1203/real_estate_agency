7fe28e17821e3f897efd8d21e00b18f2a06509ea
Teterich Ilya <teterichii@msk.bcs.ru>
2020-12-18 17:21:55 +0300

    Add inline owner in flats

--- property/admin.py
+++ property/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 
 from .models import Flat, Claim, Owner
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ["owner"]
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone',
-                    'owners_phonenumber']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['rooms_number', 'has_balcony', 'new_building']
     raw_id_fields = ["liked_by"]
+    inlines = [OwnerInline]
 
 
 class ClaimAdmin(admin.ModelAdmin):
@@ -19,6 +22,7 @@ class ClaimAdmin(admin.ModelAdmin):
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
+    search_fields = ['full_name']
 
 
 admin.site.register(Flat, FlatAdmin)
--- property/models.py
+++ property/models.py
@@ -5,14 +5,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
-    owner_pure_phone = PhoneNumberField(blank=True, null=True, verbose_name="Нормализованный номер владельца")
-    owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
-    
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
-
     town = models.CharField("Город, где находится квартира", max_length=50, db_index=True)
     town_district = models.CharField("Район города, где находится квартира", max_length=50, blank=True, help_text='Чертаново Южное')
     address = models.TextField("Адрес квартиры", help_text='ул. Подольских курсантов д.5 кв.4')
